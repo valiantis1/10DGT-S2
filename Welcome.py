7e76a12cba5a95fa2adfea8a879a676fa20d3ead
valiantis1 <valiant.jamison@kingsway.school.nz>
2024-09-18 10:25:52 +1200

    first upload of welcome file
    some basic print stuff

---
+++ Welcome.py
@@ -0,0 +1,10 @@
+# Python program
+# Author: Valiant Jamison
+# Date: 2024-09-18
+# Version: 1.0
+
+# This is a comment. It starts with a #.
+# Print welcome message
+print("Hello World!\n") #\n makes a line brake
+print() # This is a line brake
+print("Congratulations :)")
