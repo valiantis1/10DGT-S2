07148e6fc9641db3007162d37c46b9de2491d51f
valiantis1 <valiant.jamison@kingsway.school.nz>
2024-09-20 14:37:11 +1200

    first learning thingy about variables

---
+++ Variables.py
@@ -0,0 +1,26 @@
+# Showing how variables work
+# Author: Valiant Jamison
+# Date: 20/09/2024
+# Verison: 1.0
+
+
+# Different types of variables
+# Store a number
+num_1 = 80.0 # this displayes what num_1 contains
+
+# store a string
+name = "Valiant"
+print(name)
+name = "josh (gay)" # changing the variable from "valiant" to "josh (gay)"
+print(name)
+
+# Do calilations in a variables and store the result
+sum = 5 + 5 # follows PEP8 Python conventions
+print(sum)
+
+sum = "5 + 5 = 10" # this is a string
+print(sum)
+
+# Adding strings togeather is called concatenations
+concatenation = name + " " + sum
+print(concatenation)
